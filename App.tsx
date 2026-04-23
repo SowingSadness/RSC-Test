6f094b054c631bc010e3f818174465e74062584b
Kirill <sowingsadness@ya.ru>
2026-04-23 23:08:54 +0300

    Create App.tsx as React Server Component

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to RSC App</h1>
+      <p>This is a React Server Component rendered with @lazarv/rsc</p>
+    </div>
+  );
+}
